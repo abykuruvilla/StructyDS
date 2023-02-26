1636410560c03d892a7ec072ae21dd05ee424ef4
Aby Kuruvilla <abykuruvilla@hotmail.com>
2023-02-26 16:52:12 -0500

    Finished SemestersRequired problem

--- src/com/kaby/ds/graph/SemestersRequired.java
+++ src/com/kaby/ds/graph/SemestersRequired.java
@@ -1,6 +1,5 @@
 package com.kaby.ds.graph;
 
-import com.kaby.ds.graph.util.GraphUtil;
 import com.kaby.ds.helper.ResultPair;
 
 import java.util.*;
@@ -42,6 +41,7 @@ public class SemestersRequired {
         };
         int semestersRequired1 = semestersRequired(numCourses, prereqs); // -> 3
         ResultPair<Integer, Integer> resultPair = new ResultPair<>("The number of semesters required is ", 3, semestersRequired1);
+        resultPair.printResultPair();
 
 
 //        ===========================
@@ -68,6 +68,7 @@ public class SemestersRequired {
         };
         int semestersRequired2 = semestersRequired(numCourses2, prereqs2); // -> 5
         ResultPair<Integer, Integer> resultPair2 = new ResultPair<>("The number of semesters required is ", 5, semestersRequired2);
+        resultPair2.printResultPair();
 
 
     }
@@ -76,8 +77,55 @@ public class SemestersRequired {
         // To make life simple let's do an adjacency list of the prerequisites edge list
         Map<Integer, List<Integer>> graph = buildDirectedGraph(numCourses, prereqs);
 
+        // A Map to track the distance from the terminal node
+        Map<Integer, Integer> distance = new HashMap<>();
 
-        return 0;
+        // Let's Iterate over each course in the adjacency list
+        for(Integer course : graph.keySet()) {
+            // base case - finding the terminal course that will be done last
+            if(graph.get(course).isEmpty()) {
+                // The last course is at a distance of 0 from itself
+                // But since we are counting number of nodes we will initialize to 1
+                distance.put(course, 1);
+            }
+        }
+
+        // Now that we have the terminal courses, let's figure out the distance of the other courses
+        // from the terminal courses
+        // We will be doing a recursive DFS
+        for(Integer course : graph.keySet()) {
+            exploreDistance(graph, course, distance);
+        }
+
+        // We have explored all the courses and the distance of each in a component of related courses from their terminal
+        // course which has to be done last
+        // So return the max distance
+        return Collections.max(distance.values());
+    }
+
+    private static Integer exploreDistance(Map<Integer, List<Integer>> graph, Integer course, Map<Integer, Integer> distance) {
+        // if the node is already in distance, return that value directly as we have determined the distance already from a terminal course
+        if(distance.containsKey(course)) {
+            return distance.get(course);
+        }
+
+        // track the max distance
+        int maxDistance = 0;
+
+        // look through the neighbors
+        for(Integer neighborCourse : graph.get(course)) {
+            int neighborDist = exploreDistance(graph, neighborCourse, distance);
+            if(neighborDist > maxDistance) {
+                maxDistance = neighborDist;
+            }
+        }
+
+        // Update distances for this course, it is one additional to distance from the
+        // neighbor of the course
+        distance.put(course, 1 + maxDistance);
+
+        // Return the max distance for the course
+        return distance.get(course);
     }
 
     private static Map<Integer, List<Integer>> buildDirectedGraph(int numCourses, Integer[][] prereqs) {
